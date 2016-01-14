a496a64708aae70b0718a32b2072b343ade54953
Wonju Jeon <wjjeon@theworkshop.com>
2016-01-15 02:03:59 +0900

    tests for Lesson 03: Python DB-API convention fixes following PEP8

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -3,29 +3,38 @@
 # 
 
 import time
+import psycopg2
 
-## Database connection
-DB = []
 
-## Get posts from database.
 def GetAllPosts():
-    '''Get all the posts from the database, sorted with the newest first.
+    """Get all the posts from the database, sorted with the newest first.
 
     Returns:
       A list of dictionaries, where each dictionary has a 'content' key
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
-    '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+    """
+    conn = psycopg2.connect("dbname=forum")
+    cur = conn.cursor()
+    cur.execute("delete from posts where content like '%spam%';")
+    conn.commit()
+    cur.execute("select * from posts order by time desc;")
+    db = cur.fetchall()
+    cur.close()
+    conn.close()
+    posts = [{'content': str(row[0]), 'time': time.strftime('%c', row[1].timetuple())} for row in db]
     return posts
 
-## Add a post to the database.
+
 def AddPost(content):
-    '''Add a new post to the database.
+    """Add a new post to the database.
 
     Args:
       content: The text content of the new post.
-    '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    """
+    conn = psycopg2.connect("dbname=forum")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO posts VALUES (%s)", (content,))
+    conn.commit()
+    cur.close()
+    conn.close()
